667700457ceb4165e45ff7c3013682506d969dbd
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-02-19 23:19:18 +0400

    updates

--- script.js
+++ script.js
@@ -4,3 +4,9 @@
 const celsiusElement = document.getElementById("celsius");
 const fahrenheitElement = document.getElementById("fahrenheit");
 const kelvinElement = document.getElementById("kelvin");
+const btnClear = document.getElementById("btn-clear");
+const allInput = document.querySelectorAll(".input");
+
+btnClear.addEventListener("click", function () {
+  allInput.forEach((input) => (input.value = ""));
+});
