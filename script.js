81283e066158a8fd9a2ed35ba79612252f7d2f0e
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-02-20 00:14:34 +0400

    last update

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ const allInput = document.querySelectorAll(".input");
 btnClear.addEventListener("click", function () {
   allInput.forEach((input) => (input.value = ""));
 });
+
+function calculateTemperature(event) {
+  console.log(event.target.name);
+  const currentValue = +event.target.value;
+
+  switch (event.target.name) {
+    case "celsius":
+      kelvinElement.value = (currentValue + 273.15).toFixed(2);
+      fahrenheitElement.value = (currentValue * 1.8 + 32).toFixed(2);
+      break;
+    case "fahrenheit":
+      celsiusElement.value = ((currentValue - 32) / 1.8).toFixed(2);
+      kelvinElement.value = ((currentValue - 32) / (1.8 + 273.15)).toFixed(2);
+      break;
+    case "kelvin":
+      celsiusElement.value = (currentValue - 273.15).toFixed(2);
+      fahrenheitElement.value = ((currentValue - 273.15) * 1.8 + 32).toFixed(2);
+      break;
+    default:
+      break;
+  }
+}
